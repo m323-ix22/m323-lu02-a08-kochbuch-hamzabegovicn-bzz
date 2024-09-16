84119f387fcd34c35f94569b3d02fdc5bf0fb8f5
nsham <hamzabegovicn@bzz.ch>
2024-09-16 14:01:10 +0200

    done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 
 """
-Pure Function zum Laden des Rezepts von JSON zu Python-Dictionary
+Funktionen zum Bearbeiten eines JSON-basierten Kochbuchs.
 """
 
 
@@ -9,26 +9,26 @@ def load_recipe(recipe_json: str) -> dict:
     return json.loads(recipe_json)
 
 
-def adjust_recipe(recipe: dict, num_persons: int) -> dict:
-    factor = num_persons / recipe['servings']
+def adjust_recipe(recipe: dict, persons: int) -> dict:
+    factor = persons / recipe["servings"]
 
-    new_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
+    new_ingredients = {ingredient: amount * factor for ingredient, amount in recipe["ingredients"].items()}
 
     return {
         "title": recipe["title"],
         "ingredients": new_ingredients,
-        "servings": num_persons
+        "servings": persons
     }
 
 
 if __name__ == '__main__':
-    recipe_json = ('{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, '
-                   '"Minced Meat": 500}, "servings": 4}')
+    recipe_json_data = ('{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, '
+                        '"Minced Meat": 500}, "servings": 4}')
 
-    num_persons = 2
+    new_persons = 2
 
-    recipe = load_recipe(recipe_json)
+    loaded_recipe = load_recipe(recipe_json_data)
 
-    adjusted_recipe = adjust_recipe(recipe, num_persons)
+    updated_recipe = adjust_recipe(loaded_recipe, new_persons)
 
-    print(json.dumps(adjusted_recipe, indent=4))
+    print(json.dumps(updated_recipe, indent=4))
