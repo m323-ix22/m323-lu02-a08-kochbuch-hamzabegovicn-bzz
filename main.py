ed6e6df25895968033d3e48d639396fd83800a8a
nsham <hamzabegovicn@bzz.ch>
2024-09-16 13:57:32 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-# Dein Code kommt hier hin
+import json
+"""
+Pure Function zum Laden des Rezepts von JSON zu Python-Dictionary
+"""
+
+def load_recipe(recipe_json: str) -> dict:
+    return json.loads(recipe_json)
+
+
+def adjust_recipe(recipe: dict, num_persons: int) -> dict:
+    factor = num_persons / recipe['servings']
+
+    new_ingredients = {ingredient: amount * factor for ingredient, amount in recipe['ingredients'].items()}
+
+    return {
+        "title": recipe["title"],
+        "ingredients": new_ingredients,
+        "servings": num_persons
+    }
+
 
 if __name__ == '__main__':
-    # Beispiel für die Datenstruktur eines Rezepts
     recipe_json = '{"title": "Spaghetti Bolognese", "ingredients": {"Spaghetti": 400, "Tomato Sauce": 300, "Minced Meat": 500}, "servings": 4}'
-    # Dein Code kommt hier hin
+
+    num_persons = 2
+
+    recipe = load_recipe(recipe_json)
+
+    adjusted_recipe = adjust_recipe(recipe, num_persons)
+
+    print(json.dumps(adjusted_recipe, indent=4))
